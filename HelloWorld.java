78dc4bf1e51a1128b9b951f76f064dda3f105958
sgcoding2016 <sg@10past12.co>
2025-05-16 20:30:35 +0100

    Add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package first;
+
+/**
+ * This is my first code example
+ */
+public class HelloWorld {
+
+	/**
+	 * This is my first method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World MSC 2024! I am alive.\n");
+		System.out.println("Hello World! I am alive\nI can write on two lines!\n");
+		System.out.println("Hello World! I am alive\n\nI can write on two lines!\n");
+		System.out.println("     *");
+		System.out.println("    ***");
+		System.out.println("   *****");
+		System.out.println("  *******");
+		System.out.println(" *********");
+		System.out.println("Happy Christmas");
+		System.out.println(); // new black line
+
+	}
+
+}
