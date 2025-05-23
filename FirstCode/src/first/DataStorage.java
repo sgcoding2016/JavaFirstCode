ae789e54fd24a19dd96aff73fabdaa8162255b2b
sgcoding2016 <sg@10past12.co>
2025-05-23 18:30:15 +0100

    Add DataStorage.java

---
+++ FirstCode/src/first/DataStorage.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package first;
+
+/**
+ * 
+ */
+public class DataStorage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String myName;
+		myName = "Saul G";
+		int myAge;
+		myAge = 61;
+		System.out.printf("My name is %s. My age is %s.", myName, myAge);
+
+	}
+
+}
